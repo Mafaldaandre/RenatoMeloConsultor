4a39a2ff3797ae5461dd936e9fe29353e08cd7e9
mafalda.andre <mafalda.andre@proside.pt>
2024-02-01 18:08:11 +0000

    alteraçoes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Header } from "./components/header/Header";
 import { Footer } from "./components/footer/Footer";
 import { Outlet } from "react-router-dom";
 import { FavoritesProvider } from "./context/favoritesContext";
+import { ScrollToTop } from "./components/scroll/ScrollToTop";
 
 const App = () => {
   return (
     <div>
       <FavoritesProvider>
+        <ScrollToTop />
         <Header />
         <Outlet />
         <Footer />
